41b32381f41540db1a5a806a6c41979114f0dc0e
Hitoshi Uchida <uchida@w3.org>
2013-08-02 07:35:56 -0700

    Fixed scrollbar issue.

--- chrome-ex/popup.js
+++ chrome-ex/popup.js
@@ -1,7 +1,7 @@
 /* $Id$ */
 //console.log = function(v){};
 String.prototype.trim = function() {
-  return this.replace(/^\s+|\s+$/g, "");
+    return this.replace(/^\s+|\s+$/g, "");
 };
 
 var baseURL = "http://eow.alc.co.jp/search?q=";
@@ -13,54 +13,57 @@ function get(keyword) {
     req.open("GET", url, true);
     req.onreadystatechange = function() {
         if (req.readyState == 4 && this.responseText.length) {
-	    $("#progress").hide();
-	    alc_html = this.responseText;
-	    var index = alc_html.indexOf("<div id=\"resultsList\"");
-	    if(index == -1) {//not found
-		$("#message").html("<strong style='color:red;'>not found <i>"
-				   + keyword + "</i></strong>");
-		showHistory();
-		$("#container").html("");
-	    } else {
-		alc_html = alc_html.substr(index);
-		alc_html = $.parseHTML(alc_html)[0];
-		$("#message").html("");
-		$("#history").html("");
-		$("#container").html(alc_html);
-		
-		//add history
-		localStorage.setItem(keyword, new Date());
-	    }
-	}
+            $("#progress").hide();
+            $("body").scrollTop(0);
+            
+            alc_html = this.responseText;
+            var index = alc_html.indexOf("<div id=\"resultsList\"");
+            if(index == -1) {//not found
+                $("#message").html("<strong style='color:red;'>not found <i>"
+                                   + keyword + "</i></strong>");
+                showHistory();
+                $("#container").html("");
+            } else {
+                alc_html = alc_html.substr(index);
+                alc_html = $.parseHTML(alc_html)[0];
+                $("#message").html("");
+                $("#history").html("");
+                $("#container").html(alc_html);
+                
+                //add history
+                localStorage.setItem(keyword, new Date());
+            }
+        }
     };
     req.send(null);
     $("#progress").show();
 }
 function showHistory() {
     function comp(a, b) {
-	return b.d - a.d;
+        return b.d - a.d;
     }
     var words = [];
     for(var key in localStorage) {
-	var date = new Date(localStorage[key]);
-	var word = {};
-	word["s"] = key;
-	word["d"] = date;
-	words.push(word);
+        var date = new Date(localStorage[key]);
+        var word = {};
+        word["s"] = key;
+        word["d"] = date;
+        words.push(word);
     }
     words.sort(comp);
     
     var historyHTML = "<h2 class='title'>History</h2><table id='word_history' width='100%'>";
     for(var i = 0; i < words.length; i++) {
-	historyHTML += "<tr class='frame' href='" + words[i].s + "'><td title='click for search again!'>" + words[i].s +
-	    "</td><td style='float:right'>" +  words[i].d.toLocaleString() + "</td></tr>";
+        historyHTML += "<tr class='frame' href='" + words[i].s + "'><td title='click for search again!'>" + words[i].s +
+            "</td><td style='float:right'>" +  words[i].d.toLocaleString() + "</td></tr>";
     }
     historyHTML += "</table>";
     $("#history").html(historyHTML);
     $("#container").html("");
-
+    $("body").scrollTop(0);
+    
     $("#word_history tr").click(function() {
-	get($($(this)).attr("href"));
+        get($($(this)).attr("href"));
     });
 }
 document.addEventListener('DOMContentLoaded', function () {
@@ -68,16 +71,16 @@ document.addEventListener('DOMContentLoaded', function () {
     $("#search").focus();
     $("#progress").hide();
     $("#search").keypress(function(event) {
-	if(event.which == 13) {
-	    get(this.value);
-	}
+        if(event.which == 13) {
+            get(this.value);
+        }
     });
     $("#bt_clear").click(function(event) {
-	localStorage.clear();
-	showHistory();
+        localStorage.clear();
+        showHistory();
     });
     $("#bt_history").click(function(event) {
-	showHistory();
+        showHistory();
     });
     
     //show history
